faf132654819fa5ba8133363fda562263e178148
lucasandrad <lucasandrad@yandex.com>
2018-06-02 19:16:48 -0300

    Add timer start function and moment js package

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,6 +1,13 @@
 const { ipcRenderer } = require('electron');
+const path = require('path');
+const timer = require(filePath('js/timer'));
+
+function filePath(file) {
+  return path.resolve(__dirname, file);
+}
 
 let linkAbout = document.querySelector('#link-about');
+let tempo = document.querySelector('.tempo');
 
 linkAbout.addEventListener('click', function (){
   ipcRenderer.send('open-about-window');
@@ -11,4 +18,5 @@ let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
 button.addEventListener('click', () => {
   imgs = imgs.reverse();
   button.src = imgs[0];
+  timer.iniciar(tempo);
 });
