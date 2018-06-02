f4f8fad13c2721de1ebefe7e0c6db5a41a6e4b2c
lucasandrad <lucasandrad@yandex.com>
2018-06-02 12:37:26 -0300

    Change default behavior of about page

--- app/js/about.js
+++ app/js/about.js
@@ -1,7 +1,18 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, shell } = require('electron');
+const process = require('process');
 
 let linkClose = document.querySelector('#linkClose');
+let linkGithub = document.querySelector('#linkGithub')
+let electronVersion = document.querySelector('#electronVersion');
 
 linkClose.addEventListener('click', function() {
   ipcRenderer.send('close-about-windown');
 });
+
+linkGithub.addEventListener('click', () => {
+  shell.openExternal("https://github.com/lucasandrad");
+});
+
+window.onload = function() {
+  electronVersion.textContent = process.versions.electron;
+}
