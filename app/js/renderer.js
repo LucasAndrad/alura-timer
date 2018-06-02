013ad2d628b00e9b287afbfbbfd904a73eeb694e
lucasandrad <lucasandrad@yandex.com>
2018-06-02 13:48:21 -0300

    Add switch function to change image between play and stop

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -5,3 +5,10 @@ let linkAbout = document.querySelector('#link-about');
 linkAbout.addEventListener('click', function (){
   ipcRenderer.send('open-about-window');
 });
+
+let button = document.querySelector('.botao-play');
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
+button.addEventListener('click', () => {
+  imgs = imgs.reverse();
+  button.src = imgs[0];
+});
